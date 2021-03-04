6bdb0a639dd45b22da82a2e4241b2a1f632cd275
Carlos Eduardo <carloshdz.udb@gmail.com>
2021-03-03 19:43:11 -0600

    Mostrando opciones

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const { leerInput } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
 
 require("colors")
 
 const main = async() => {
-    const text = await leerInput("Hola: ");
-    console.log(text);
+    let opt;
+
+    do{
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                console.log("Opcion 1")
+                break;
+            
+            case 2:
+                console.log("Opcion 1")
+                break;
+        
+            default:
+                break;
+        }
+        
+        await pausa();
+
+    }while( opt !== "0");
+
 }
 
 main();
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -10,31 +10,15 @@ const inquirerMenu  = async() => {
             message: '¿Que desea hacer?',
             choices: [
                 {
-                    value:'1',
-                    name:`${'1.'.green} Crear tarea`
+                    value:1,
+                    name:`${'1.'.green} Buscar ciudad`
                 },
                 {
-                    value:'2',
-                    name:`${'2.'.green} Listar tareas`
-                },
+                    value:2,
+                    name:`${'2.'.green} Historial`
+                },             
                 {
-                    value:'3',
-                    name:`${'3.'.green} Listar tareas completadas`
-                },
-                {
-                    value:'4',
-                    name:`${'4.'.green} Lista tareas pendientes`
-                },
-                {
-                    value:'5',
-                    name:`${'5.'.green} Completar tareas`
-                },
-                {
-                    value:'6',
-                    name:`${'6.'.green} Borrar Tarea`
-                },
-                {
-                    value:'0',
+                    value:0,
                     name:`${'0.'.green} Salir`
                 }       
             ]
