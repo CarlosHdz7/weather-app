094f10a8993af577281158f4bbd03a03c0e7997e
Carlos Eduardo <carloshdz.udb@gmail.com>
2021-03-03 19:36:35 -0600

    Agregando menu inquierer

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const { leerInput } = require("./helpers/inquirer")
+
 require("colors")
+
+const main = async() => {
+    const text = await leerInput("Hola: ");
+    console.log(text);
+}
+
+main();
